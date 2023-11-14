f98e97264fefee5c1b40426fd4fd2e7937ade8d9
Curtis Myzie <curtis.myzie@gmail.com>
2023-11-14 15:54:13 -0500

    Improve naming

--- connect.go
+++ connect.go
@@ -8,15 +8,16 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func RetryConnect(ctx context.Context, url string) (*pgx.Conn, error) {
+func ConnectWithRetries(ctx context.Context, url string) (*pgx.Conn, error) {
 	for {
 		conn, err := pgx.Connect(ctx, url)
 		if err == nil {
 			return conn, nil
 		}
-		if strings.Contains(err.Error(), "connection refused") ||
-			strings.Contains(err.Error(), "no such host") ||
-			strings.Contains(err.Error(), "password authentication failed") {
+		errMsg := err.Error()
+		if strings.Contains(errMsg, "connection refused") ||
+			strings.Contains(errMsg, "no such host") ||
+			strings.Contains(errMsg, "password authentication failed") {
 			time.Sleep(time.Second * 3)
 			continue
 		}
